5fea7991135384d6eb723843377b50fa5197ee1d
trummelbummel <theresa.fruhwuerth@gmail.com>
2022-05-13 17:14:59 +0200

    Add files for udacity project
    Add all files for azure project.

---
+++ train.py
@@ -0,0 +1,85 @@
+from sklearn.linear_model import LogisticRegression
+import argparse
+
+import os
+import numpy as np
+from sklearn.metrics import mean_squared_error
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
+from azureml.core.run import Run
+from azureml.data.dataset_factory import TabularDatasetFactory
+from azureml.core import Dataset, Datastore
+from azureml.data.datapath import DataPath
+
+def stratified_sample(df, col, n_samples):
+    n = min(n_samples, df[col].value_counts().min())
+    df_ = df.groupby(col).apply(lambda x: x.sample(n))
+    df_.index = df_.index.droplevel(0)
+    return df_
+
+def clean_data(data):
+    # Dict for cleaning data
+    months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
+    weekdays = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":7}
+
+
+    # Clean and one hot encode data
+    x_df = data.to_pandas_dataframe().dropna()
+    jobs = pd.get_dummies(x_df.job, prefix="job")
+    x_df.drop("job", inplace=True, axis=1)
+    x_df = x_df.join(jobs)
+    x_df["marital"] = x_df.marital.apply(lambda s: 1 if s == "married" else 0)
+    x_df["default"] = x_df.default.apply(lambda s: 1 if s == "yes" else 0)
+    x_df["housing"] = x_df.housing.apply(lambda s: 1 if s == "yes" else 0)
+    x_df["loan"] = x_df.loan.apply(lambda s: 1 if s == "yes" else 0)
+    contact = pd.get_dummies(x_df.contact, prefix="contact")
+    x_df.drop("contact", inplace=True, axis=1)
+    x_df = x_df.join(contact)
+    education = pd.get_dummies(x_df.education, prefix="education")
+    x_df.drop("education", inplace=True, axis=1)
+    x_df = x_df.join(education)
+    x_df["month"] = x_df.month.map(months)
+    x_df["day_of_week"] = x_df.day_of_week.map(weekdays)
+    x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
+
+    x_df = stratified_sample(x_df, 'y', n_samples=4000)
+
+    y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
+    return x_df, y_df
+
+def main():
+    # Add arguments to script
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
+    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
+
+    args = parser.parse_args()
+
+    run = Run.get_context()
+
+    run.log("Regularization Strength:", np.float(args.C))
+    run.log("Max iterations:", np.int(args.max_iter))
+
+    # TODO: Create TabularDataset using TabularDatasetFactory
+    # Data is located at:
+    # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+    # default behavior: support_multi_line=False
+    ds = Dataset.Tabular.from_delimited_files(path='https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv')
+
+    x, y = clean_data(ds)
+    # TODO: Split data into train and test sets.
+
+    X_train, X_test, y_train, y_test = train_test_split(
+            x, y, test_size=0.33, random_state=42)
+    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(X_train, y_train)
+
+    accuracy = model.score(X_test, y_test)
+    run.log("accuracy", np.float(accuracy))
+    joblib.dump(value=model, filename="logistic_regression.pkl")
+    return 
+
+if __name__ == '__main__':
+    main()
